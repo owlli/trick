fbde618c7c8cc6ab2ee634b6ae81c750f3cc3ca6
lzh <phoenix_lzh@sina.com>
2018-06-19 00:17:50 +0800

    modify xray.c

--- desktoptrick/xray.c
+++ desktoptrick/xray.c
@@ -16,37 +16,7 @@ static void alrm_sa(int s,siginfo_t *infop,void *unused)
 {
 	if(infop->si_code != SI_KERNEL)
 		return ;
-
-	unsigned long ln = LINESIZE;
-	FILE *df,*sf;
-	sf = fopen(SFILE,"r");
-	df = fopen(DFILE,"w+");
-	int i,n;
-	char *l;
-	srand(time(NULL));
-	int x;
-
-	n = rand() % 5;
-//	printf("n = %d\n",n);
-
-	for(i = 0;i < n;i++)
-	{
-		x = getline(&l,&ln,sf);
-//		printf("ln = %ld\n",ln);
-//		printf("x = %d\n",x);
-//		printf("l = %s\n",l);
-
-		//memset(l,0,LINESIZE);
-	}
-//	printf("finish for\n");
-	x = getline(&l,&ln,sf);
-
-//		printf("l = %s\n",l);
-	fwrite(l,1,x,df);
-
-	fclose(sf);
-	fclose(df);
-
+	return ;
 }
 
 
@@ -56,8 +26,6 @@ int main()
 	pid = fork();
 	if(pid == 0)
 	{
-		do
-		{
 		setsid();
 		struct itimerval itv;
 		itv.it_interval.tv_sec = 5;
@@ -69,17 +37,47 @@ int main()
 			perror("setitimer()");
 			exit(1);
 		}
-
 		struct sigaction sa,oldsa;
 		sa.sa_sigaction = alrm_sa;
 		sigemptyset(&sa.sa_mask);
 		sa.sa_flags = SA_SIGINFO;
 		sigaction(SIGALRM,&sa,&oldsa);
-		pause();
-		}while(1);
-//	printf("end \n");
+		do
+		{
+			pause();
+			unsigned long ln = LINESIZE;
+			FILE *df,*sf;
+			sf = fopen(SFILE,"r");
+			df = fopen(DFILE,"w+");
+			int i,n;
+			char *l;
+			srand(time(NULL));
+			int x;
+			n = rand() % 5;
+			//	printf("n = %d\n",n);
 
-	exit(0);
+			for(i = 0;i < n;i++)
+			{
+				x = getline(&l,&ln,sf);
+				//		printf("ln = %ld\n",ln);
+				//		printf("x = %d\n",x);
+				//		printf("l = %s\n",l);
+
+				//memset(l,0,LINESIZE);
+			}
+			//	printf("finish for\n");
+			x = getline(&l,&ln,sf);
+
+			//		printf("l = %s\n",l);
+			fwrite(l,1,x,df);
+
+			fclose(sf);
+			fclose(df);
+		}while(1);
+		//	printf("end \n");
+
+		sigaction(SIGALRM,&oldsa,NULL);
+		exit(0);
 	}
 
 }
